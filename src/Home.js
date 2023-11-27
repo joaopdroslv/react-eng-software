2dc6c8f41384d5681f004f482ad66266668e5204
JOÃO PEDRO <142755923+joaopdroslv@users.noreply.github.com>
2023-11-27 00:31:40 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      <h1>Home page!</h1>
+      <h1 className="App">Home page!</h1>
       <nav>
         <ul>
           <li>
