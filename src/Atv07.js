62f469edd5e549903d8bde94aa10f363524eb550
JOÃO PEDRO <142755923+joaopdroslv@users.noreply.github.com>
2023-11-26 21:47:01 -0300

    Update Atv07.js

--- src/Atv07.js
+++ src/Atv07.js
@@ -2,7 +2,8 @@ import Cards from "./Components/MemoryGame/Cards.jsx";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Link to="/">Home page</Link>
       <h1>Memory Game</h1>
       <Cards />
     </div>
