5fc6f88a7f3adca880bb657617becfdeea567a4b
JOÃO PEDRO <142755923+joaopdroslv@users.noreply.github.com>
2023-11-26 21:49:22 -0300

    Add files via upload

---
+++ src/Atv04.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Atv01() {
+  return (
+    <div>
+      <h1>Atividade 04</h1>
+      <Link to="/">Home page</Link>
+    </div>
+  );
+}
