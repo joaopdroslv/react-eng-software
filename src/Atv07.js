5baa9bd40261fe339b6726cf69e53e05a8f6eb82
JOÃO PEDRO <142755923+joaopdroslv@users.noreply.github.com>
2023-11-26 21:45:47 -0300

    Add files via upload

---
+++ src/Atv07.js
@@ -0,0 +1,12 @@
+import Cards from "./Components/MemoryGame/Cards.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
