ec879e458f56d9306f86786303862d472e3c4d75
JOÃO PEDRO <142755923+joaopdroslv@users.noreply.github.com>
2023-11-26 23:52:59 -0300

    Update Atv06.js

--- src/Atv06.js
+++ src/Atv06.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import Calculator from "./Components/Calculator/Calculator";
 
 export default function Atv06() {
   return (
     <div>
       <h1>Atividade 06</h1>
       <Link to="/">Home page</Link>
+      <Calculator />
     </div>
   );
 }
