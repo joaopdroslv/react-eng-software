7c183c596aac0c0fd551422e1d1838225a7813e9
JOÃO PEDRO <142755923+joaopdroslv@users.noreply.github.com>
2023-11-26 21:51:50 -0300

    Update Atv01.js

--- src/Atv01.js
+++ src/Atv01.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Atv01() {
   return (
     <div>
-      <h1>Atividade 01</h1>
+      <h1>Relógio</h1>
       <Link to="/">Home page</Link>
     </div>
   );
