b12a6f982b6f681ac172cdc5cef181eb1e0c41bb
JOÃO PEDRO <142755923+joaopdroslv@users.noreply.github.com>
2023-11-26 21:52:04 -0300

    Update Atv02.js

--- src/Atv02.js
+++ src/Atv02.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Atv02() {
   return (
     <div>
-      <h1>Atividade 02</h1>
+      <h1>Letreiro</h1>
       <Link to="/">Home page</Link>
     </div>
   );
